96525da82f8d4b3d8db21fd57d227555ce024ac1
Raimundas Banevicius <raima156@yahoo.com>
2018-02-26 16:29:26 +0200

    Practice lists added.

---
+++ practiceLists/practiceLists/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+
+    private string hugeText = "TODO - didelis tekstas cia!";
+
+    static void Main(string[] args)
+    {
+
+        //TODO - Sukurit funkcija kuri is string teksto padaro zodziu List, getWordsOutOfText(...);
+
+        // TODO - sulast isgiausius zodzius, findLongestWordInList(...);
+        // TODO juos atspausdiniti
+
+        // TODO - sulast zodi "split", findWordSplitInList(...);
+
+        // TODO - supeneruoti nauja atsitiktini straipsni, generateTextOutOfWords(...)
+        // TODO ji atspausdiniti.
+
+    }
+
+}
